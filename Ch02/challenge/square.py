e8c227c95139f27b3a9cbd48b81d916b20a8f236
shschmahl42 <Sam.Schmahl@gmail.com>
2025-08-15 10:48:21 -0500

    Ch 1,2 (all) updates

--- Ch02/challenge/square.py
+++ Ch02/challenge/square.py
@@ -15,3 +15,12 @@ img.shape
 # Top-left: 190x350
 # Bottom-right: 680x850
 # Line width: 5
+
+color = [0,0, 0xFF]
+
+img[350:350+5, 190:680] = color
+img[850:850+5, 190:680] = color
+img[350:850, 190:190+5] = color
+img[350:850, 680-5:680] = color
+
+plt.imshow(img)
